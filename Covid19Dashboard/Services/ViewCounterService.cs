d83a6ea990606075811c121c8ec4ee0c2da2828d
Max Reble <39700092+maxreb@users.noreply.github.com>
2020-11-26 14:13:18 +0100

    adds unique views

--- Covid19Dashboard/Services/ViewCounterService.cs
+++ Covid19Dashboard/Services/ViewCounterService.cs
@@ -16,8 +16,10 @@ namespace Covid19Dashboard.Services
 	public sealed class ViewCounterService : IDisposable
 	{
 		const string databaseFilePath = "data/views/views.json";
+		const string databaseUniqueFilePath = "data/views/unique_views.json";
 
 		private readonly ConcurrentDictionary<DateTime, int> _viewsPerDate;
+		private readonly ConcurrentDictionary<DateTime, ConcurrentDictionary<string, int>> _uniqueViews;
 		private readonly Timer _timer;
 		private readonly ILogger<ViewCounterService> _logger;
 
@@ -25,25 +27,39 @@ namespace Covid19Dashboard.Services
 		public ViewCounterService(ILogger<ViewCounterService> logger)
 		{
 			var dir = Path.GetDirectoryName(databaseFilePath)!;
-			ConcurrentDictionary<DateTime, int>? dict = null;
 			if (!Directory.Exists(dir))
 				Directory.CreateDirectory(dir);
-			if (File.Exists(databaseFilePath))
-			{
-				var json = File.ReadAllText(databaseFilePath);
-				dict = JsonSerializer.Deserialize<ConcurrentDictionary<DateTime, int>>(json);
-			}
 
-			_viewsPerDate = dict ?? new ConcurrentDictionary<DateTime, int>();
+			ReadAndParse(out _viewsPerDate, databaseFilePath);
+			ReadAndParse(out _uniqueViews, databaseUniqueFilePath);
 
 			_timer = new Timer(WriteToFile, null, TimeSpan.FromMinutes(1), TimeSpan.FromHours(1));
 			_logger = logger;
 		}
-		public void NewView()
+
+
+		private static void ReadAndParse<T>(out T data, string filePath) where T : class, new()
+		{
+			T? res = null;
+			if (File.Exists(filePath))
+			{
+				var json = File.ReadAllText(filePath);
+				res = JsonSerializer.Deserialize<T>(json);
+			}
+			data = res ?? new T();
+		}
+
+		public void NewView(string id)
 		{
 			var now = DateTime.Now.Date;
-			var cnt = _viewsPerDate.GetOrAdd(now, 0);
+
+			var unique = _uniqueViews.GetOrAdd(now, _ => new ConcurrentDictionary<string, int>());
+			var cnt = unique.GetOrAdd(id, 0);
+			_uniqueViews[now][id] = cnt + 1;
+
+			cnt = _viewsPerDate.GetOrAdd(now, 0);
 			_viewsPerDate[now] = cnt + 1;
+
 			newViewsSinceLastWrite = true;
 		}
 
@@ -57,8 +73,22 @@ namespace Covid19Dashboard.Services
 			var b = TryGetViewsFromDay(date, out int views);
 			return b ? views : 0;
 		}
+		private bool TryGetUniqueViewsFromDay(DateTime date, out int views)
+		{
+			date = date.Date;
+			var res = _uniqueViews.TryGetValue(date, out var val);
+			views = val?.Count ?? 0;
+			return res;
+		}
+
+		public int GetUniqueViewsFromDay(DateTime date)
+		{
+			TryGetUniqueViewsFromDay(date, out int views);
+			return views;
+		}
 
 		public int GetTodaysViews() => GetViewsFromDay(DateTime.Today);
+		public int GetTodaysUniqueViews() => GetUniqueViewsFromDay(DateTime.Today);
 		public int GetTotalViews() => _viewsPerDate.Sum(t => t.Value);
 
 		public IReadOnlyDictionary<DateTime, int> GetAllViews()
@@ -75,6 +105,8 @@ namespace Covid19Dashboard.Services
 				newViewsSinceLastWrite = false;
 				var json = JsonSerializer.Serialize(_viewsPerDate);
 				File.WriteAllText(databaseFilePath, json);
+				json = JsonSerializer.Serialize(_uniqueViews);
+				File.WriteAllText(databaseUniqueFilePath, json);
 			}
 			catch (Exception ex)
 			{
