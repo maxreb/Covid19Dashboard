0853b080aacc74ea27377a94987fe01936e5e312
Max Reble <39700092+maxreb@users.noreply.github.com>
2021-11-22 20:42:43 +0100

    make WSL docker work (without docker desktop)

--- build-docker.bat
+++ build-docker.bat
@@ -1,5 +1,11 @@
 @echo off
 cd ..
-docker build --no-cache -t hse-server01:5000/reble/covid19dashboard -f .\Covid19Dashboard\Covid19Dashboard\Dockerfile .
-docker push hse-server01:5000/reble/covid19dashboard
+where docker >nul 2>nul
+IF ERRORLEVEL 0 (
+    bash -c "docker build --no-cache -t hse-server01:5000/reble/covid19dashboard -f ./Covid19Dashboard/Covid19Dashboard/Dockerfile ."
+	bash -c "docker push hse-server01:5000/reble/covid19dashboard"
+) ELSE (
+	docker build --no-cache -t hse-server01:5000/reble/covid19dashboard -f .\Covid19Dashboard\Covid19Dashboard\Dockerfile .
+	docker push hse-server01:5000/reble/covid19dashboard
+)
 pause
